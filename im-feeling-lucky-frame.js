8140fd38ddb6c91488a3ba5e8f47e59be15c89ad
jams2blues <jams2blues@gmail.com>
2024-03-29 11:44:10 -0500

    updated styles.css and port for local vercel dev testing to 3001 instead of 3000

--- im-feeling-lucky-frame.js
+++ im-feeling-lucky-frame.js
@@ -20,7 +20,7 @@ app.get('/lucky', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
