49bec3b9b4f982a8bfa1bdc04bf70daebaf23de2
jams2blues <jams2blues@gmail.com>
2024-03-29 15:59:49 -0500

    troubleshooting

--- api/script.js
+++ api/script.js
@@ -10,53 +10,26 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
-// Include the Express module
-const express = require('express');
 const axios = require('axios');
 const path = require('path');
 
-// Create an Express application
-const app = express();
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-app.get('/api/lucky', async (req, res) => {
+module.exports = async (req, res) => {
     try {
         const jokeApiResponse = await axios.get('https://v2.jokeapi.dev/joke/Any', {
             params: {
-                blacklistFlags: 'racist', // Add any other parameters you wish to include
+                blacklistFlags: 'racist',
             }
         });
 
-        // Check the structure of the joke and respond accordingly
         if (jokeApiResponse.data.type === 'single') {
             res.json({ joke: jokeApiResponse.data.joke });
         } else if (jokeApiResponse.data.type === 'twopart') {
             res.json({ setup: jokeApiResponse.data.setup, delivery: jokeApiResponse.data.delivery });
         } else {
-            // Respond with a generic message if no joke was found
             res.json({ message: 'No joke found, try again!' });
         }
     } catch (error) {
-        // Log the error and send a 500 server error status code with a message
         console.error('Error fetching joke:', error);
         res.status(500).json({ message: 'Failed to fetch joke, please try again later.' });
     }
-});
-
-// Serve the HTML file for all other routes to enable client-side routing
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
-
-// Start the server only if it's not running through Vercel
-if (!process.env.VERCEL) {
-    const PORT = process.env.PORT || 3001; // Default to port 3001 if no environment port is specified
-    app.listen(PORT, () => {
-        console.log(`Server is up and running on port ${PORT}`);
-    });
-}
-
-// Export the app for Vercel
-module.exports = app;
+};
